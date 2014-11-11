42091810ffb025097495c388f112d9248267b2d3
Michal Marek <mmarek@suse.cz>
2014-11-11 16:37:33 +0100

    Build the KOTD against the SP3 Update project

--- rpm/config.sh
+++ rpm/config.sh
@@ -3,5 +3,5 @@ SRCVERSION=3.0
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # buildservice projects to build the kernel against
-OBS_PROJECT=SUSE:SLE-11:SP3
-IBS_PROJECT=SUSE:SLE-11-SP3:GA
+OBS_PROJECT=SUSE:SLE-11:SP3:Update
+IBS_PROJECT=SUSE:SLE-11-SP3:Update
