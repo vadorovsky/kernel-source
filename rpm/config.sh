23afb6d793a2a69510d6b24ba9447aceecea7373
Michal Marek <mmarek@suse.com>
2016-01-08 16:19:16 +0100

    Build the KOTD against SP4

--- rpm/config.sh
+++ rpm/config.sh
@@ -3,8 +3,5 @@ SRCVERSION=3.0
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # buildservice projects to build the kernel against
-#
-# There is no SP4 project in OBS yet
-#
-OBS_PROJECT=SUSE:SLE-11:SP3:Update
-IBS_PROJECT=SUSE:SLE-11-SP4:GA
+OBS_PROJECT=SUSE:SLE-11:SP4
+IBS_PROJECT=SUSE:SLE-11-SP4:Update
