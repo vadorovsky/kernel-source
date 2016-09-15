6eb17974c34839641208d9f832100fff90bbe21f
Michal Marek <mmarek@suse.cz>
2016-09-15 15:53:28 +0200

    Revert "rpm/config.sh: Build the KOTD against Leap 42.2"
    The openSUSE:Leap:42.2 project does not support the same set of
    architectures. Fix the build issue in the KOTD project instead.

--- rpm/config.sh
+++ rpm/config.sh
@@ -3,5 +3,5 @@ SRCVERSION=4.4
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # buildservice projects to build the kernel against
-OBS_PROJECT=openSUSE:Leap:42.2:Update
+OBS_PROJECT=SUSE:SLE-12-SP1:Update
 IBS_PROJECT=SUSE:SLE-12-SP2:Update
