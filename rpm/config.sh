b7c900635cf435aad44841e9962c720803e012ad
Michal Marek <mmarek@suse.cz>
2016-11-15 16:42:15 +0100

    rpm/config.sh: Build against SP2 in the OBS as well

--- rpm/config.sh
+++ rpm/config.sh
@@ -3,5 +3,5 @@ SRCVERSION=4.4
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # buildservice projects to build the kernel against
-OBS_PROJECT=SUSE:SLE-12-SP1:Update
+OBS_PROJECT=SUSE:SLE-12-SP2:Update
 IBS_PROJECT=SUSE:SLE-12-SP2:Update
