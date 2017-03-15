e0f1245166b01c33f40639a433c8828117fa0806
Michal Marek <mmarek@suse.cz>
2017-03-15 09:46:41 +0100

    Create an openSUSE-42.3 branch
    armv7 needs updating.

--- rpm/config.sh
+++ rpm/config.sh
@@ -3,5 +3,4 @@ SRCVERSION=4.4
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # buildservice projects to build the kernel against
-OBS_PROJECT=SUSE:SLE-12-SP2:Update
-IBS_PROJECT=SUSE:SLE-12-SP3:Update
+OBS_PROJECT=openSUSE:Leap:42.3:Update
