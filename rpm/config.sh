e6f1ae6115eb1d5b724fc52822f9cd926df89122
Michal Suchanek <msuchanek@suse.de>
2017-10-05 23:01:20 +0200

    Build in Leap project.

--- rpm/config.sh
+++ rpm/config.sh
@@ -3,11 +3,10 @@ SRCVERSION=4.12
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # buildservice projects to build the kernel against
-OBS_PROJECT=openSUSE:Factory
+OBS_PROJECT=openSUSE:Leap:15.0
 OBS_PROJECT_ARM=openSUSE:Factory:ARM
 OBS_PROJECT_PPC=openSUSE:Factory:PowerPC
 OBS_PROJECT_S390=openSUSE:Factory:zSystems
-IBS_PROJECT=SUSE:SLE-15:GA
 # Bugzilla info
 BUGZILLA_SERVER="apibugzilla.suse.com"
 BUGZILLA_PRODUCT="openSUSE Tumbleweed"
