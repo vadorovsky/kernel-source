263c66f4cdc63781ba6d207c007e7a015134041e
Michal Suchanek <msuchanek@suse.de>
2018-05-07 12:53:56 +0200

    rpm/config.sh: build against SP3 in OBS as well.

--- rpm/config.sh
+++ rpm/config.sh
@@ -3,7 +3,7 @@ SRCVERSION=4.4
 # variant of the kernel-source package, either empty or "-rt"
 VARIANT=
 # buildservice projects to build the kernel against
-OBS_PROJECT=SUSE:SLE-12-SP2:Update
+OBS_PROJECT=SUSE:SLE-12-SP3:Update
 IBS_PROJECT=SUSE:SLE-12-SP3:Update
 # Check the sorted patches section of series.conf
 SORT_SERIES=yes
